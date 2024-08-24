05b884e7bf08e51f5068281a7c349b78e62db2b6
David Cannan <104325852+Cdaprod@users.noreply.github.com>
2024-08-24 13:26:24 -0400

    Update repocate.go with default container logic and progress bar.

--- cmd/repocate/repocate.go
+++ cmd/repocate/repocate.go
@@ -1,9 +1,12 @@
 package repocate
 
 import (
-    "github.com/spf13/cobra"
     "fmt"
+    "os"
+    "github.com/spf13/cobra"
     "github.com/fatih/color"
+    "github.com/schollz/progressbar/v3"
+    "github.com/cdaprod/repocate/internal/container"
 )
 
 // rootCmd is the root command for the CLI
@@ -11,12 +14,16 @@ var rootCmd = &cobra.Command{
     Use:   "repocate",
     Short: "Repocate is a tool for managing development environments using Docker containers.",
     Long:  `Repocate allows you to clone repositories, create isolated development environments, and manage them using Docker containers.`,
+    Run: func(cmd *cobra.Command, args []string) {
+        // Check and run default container logic
+        handleDefaultContainer()
+    },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 func Execute() error {
     // Display ASCII art banner with color
-    color.Cyan(`   ______                           _       
+    color.Cyan(`  ______                           _       
  | ___ \                         | |      
  | |_/ /___ _ __   ___   ___ __ _| |_ ___ 
  |    // _ \ '_ \ / _ \ / __/ _` + "`" + ` | __/ _ \
@@ -60,4 +67,82 @@ func init() {
     rootCmd.AddCommand(ListCmd)
     rootCmd.AddCommand(VersionCmd)
     rootCmd.AddCommand(HelpCmd)
+}
+
+// Function to handle the default container logic
+func handleDefaultContainer() {
+    color.Cyan("Checking for 'repocate-default' container...")
+
+    bar := progressbar.NewOptions(100,
+        progressbar.OptionSetDescription("[green]Checking container status...[reset]"),
+        progressbar.OptionSetWriter(os.Stderr),
+        progressbar.OptionShowBytes(false),
+        progressbar.OptionShowCount(),
+        progressbar.OptionOnCompletion(func() {
+            fmt.Fprint(os.Stderr, "\n")
+        }),
+    )
+
+    // Simulate progress for checking container status
+    for i := 0; i < 100; i++ {
+        bar.Add(1)
+        time.Sleep(10 * time.Millisecond)
+    }
+
+    // Check if the default container exists
+    containerExists, err := container.CheckContainerExists("repocate-default")
+    if err != nil {
+        fmt.Println(color.RedString("Error checking container: %s", err))
+        os.Exit(1)
+    }
+
+    if !containerExists {
+        color.Yellow("Default container 'repocate-default' not found. Creating it now...")
+
+        // Show progress for container creation
+        bar = progressbar.NewOptions(100,
+            progressbar.OptionSetDescription("[green]Creating container...[reset]"),
+            progressbar.OptionSetWriter(os.Stderr),
+            progressbar.OptionShowBytes(false),
+            progressbar.OptionShowCount(),
+            progressbar.OptionOnCompletion(func() {
+                fmt.Fprint(os.Stderr, "\n")
+            }),
+        )
+
+        for i := 0; i < 100; i++ {
+            bar.Add(1)
+            time.Sleep(10 * time.Millisecond)
+        }
+
+        err := container.CreateAndStartContainer("repocate-default")
+        if err != nil {
+            fmt.Println(color.RedString("Error creating default container: %s", err))
+            os.Exit(1)
+        }
+        fmt.Println(color.GreenString("Default container 'repocate-default' created and started."))
+    } else {
+        color.Green("Default container 'repocate-default' exists. Executing into it now...")
+
+        bar = progressbar.NewOptions(100,
+            progressbar.OptionSetDescription("[green]Executing into container...[reset]"),
+            progressbar.OptionSetWriter(os.Stderr),
+            progressbar.OptionShowBytes(false),
+            progressbar.OptionShowCount(),
+            progressbar.OptionOnCompletion(func() {
+                fmt.Fprint(os.Stderr, "\n")
+            }),
+        )
+
+        for i := 0; i < 100; i++ {
+            bar.Add(1)
+            time.Sleep(10 * time.Millisecond)
+        }
+
+        err := container.ExecIntoContainer("repocate-default")
+        if err != nil {
+            fmt.Println(color.RedString("Error executing into default container: %s", err))
+            os.Exit(1)
+        }
+    }
 }
